12385b0777b406740a9ae597fa42d86c9997f66b
Kurt Vincent Timajo <djkurttimajo@gmail.com>
2023-12-06 18:33:45 +0800

    feat: update and delete pet

--- src/pages/Admin/ManagePets.jsx
+++ src/pages/Admin/ManagePets.jsx
@@ -4,29 +4,30 @@ import { Link } from 'react-router-dom';
 import Button from '../../components/Button';
 import { FaRegEdit } from 'react-icons/fa';
 import { MdDeleteOutline } from 'react-icons/md';
-import { GetPets } from '../../services/Pet/pet.services';
+import {
+	DeletePetById,
+	GetPets,
+	UpdatePetById,
+} from '../../services/pet.services';
 
 const ManagePets = () => {
 	const [showModal, setShowModal] = useState(false);
+	const [showDeleteModal, setShowDeleteModal] = useState(false);
+	const [deleteSuccess, setDeleteSuccess] = useState(false);
+
+	const [idToEdit, setIdToEdit] = useState(null);
+	const [idToDelete, setIdToDelete] = useState(null);
+
 	const [pets, setPets] = useState([]);
-	const [formData, setFormData] = useState({
-		name: '',
-		animalType: '',
-		breed: '',
-		age: '',
-		adoptionFee: '',
-	});
+	const [petDetails, setPetDetails] = useState({});
+	const [updates, setUpdates] = useState({});
 
 	const toggleModal = () => {
 		setShowModal(!showModal);
 	};
 
-	const handleInputChange = (e) => {
-		const { name, value } = e.target;
-		setFormData({
-			...formData,
-			[name]: value,
-		});
+	const toggleDeleteModal = () => {
+		setShowDeleteModal(!showDeleteModal);
 	};
 
 	const handleSubmit = (e) => {
@@ -35,15 +36,44 @@ const ManagePets = () => {
 
 	const getPets = async () => {
 		const res = await GetPets();
-		console.log(res);
+
 		if (res.status === 200) {
 			setPets(res.data.data.pets);
+		}
+	};
+
+	const saveUpdate = async () => {
+		const res = await UpdatePetById(petDetails[0]._id, updates);
+		if (res.status === 200) {
+			setShowModal(false);
+			setUpdates({});
+			getPets();
+		}
+	};
+
+	const okay = () => {
+		toggleDeleteModal();
+		setDeleteSuccess(false);
+		setIdToDelete(null);
+		getPets();
+	};
+
+	const deletePet = async () => {
+		const res = await DeletePetById(idToDelete);
+
+		if (res.status === 200) {
+			setDeleteSuccess(true);
 		}
 	};
 
 	useEffect(() => {
 		getPets();
 	}, []);
+
+	useEffect(() => {
+		setUpdates({});
+		setPetDetails(pets.filter((pet) => pet._id === idToEdit));
+	}, [idToEdit]);
 
 	return (
 		<div className="relative overflow-x-auto">
@@ -58,6 +88,7 @@ const ManagePets = () => {
 								&times;
 							</span>
 							<h2 className="text-lg font-bold mb-4">Update Pet Information</h2>
+
 							<form onSubmit={handleSubmit}>
 								<div className="mb-4">
 									<label
@@ -70,8 +101,13 @@ const ManagePets = () => {
 										type="text"
 										id="name"
 										name="name"
-										value={formData.name}
-										onChange={handleInputChange}
+										placeholder={petDetails[0]?.name}
+										onChange={(e) =>
+											setUpdates({
+												...updates,
+												name: e.target.value,
+											})
+										}
 										className="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500"
 									/>
 								</div>
@@ -84,10 +120,15 @@ const ManagePets = () => {
 									</label>
 									<input
 										type="text"
-										id="animaltype"
-										name="animaltype"
-										value={formData.animalType}
-										onChange={handleInputChange}
+										id="animalType"
+										name="animalType"
+										placeholder={petDetails[0]?.animalType}
+										onChange={(e) =>
+											setUpdates({
+												...updates,
+												animalType: e.target.value,
+											})
+										}
 										className="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500"
 									/>
 								</div>
@@ -102,8 +143,13 @@ const ManagePets = () => {
 										type="text"
 										id="breed"
 										name="breed"
-										value={formData.breed}
-										onChange={handleInputChange}
+										placeholder={petDetails[0]?.breed}
+										onChange={(e) =>
+											setUpdates({
+												...updates,
+												breed: e.target.value,
+											})
+										}
 										className="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500"
 									/>
 								</div>
@@ -118,8 +164,13 @@ const ManagePets = () => {
 										type="text"
 										id="age"
 										name="age"
-										value={formData.age}
-										onChange={handleInputChange}
+										placeholder={petDetails[0]?.age}
+										onChange={(e) =>
+											setUpdates({
+												...updates,
+												age: e.target.value,
+											})
+										}
 										className="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500"
 									/>
 								</div>
@@ -128,18 +179,24 @@ const ManagePets = () => {
 										htmlFor="adoptionfee"
 										className="block text-gray-700 font-bold mb-2"
 									>
-										Age
+										Adoption Fee
 									</label>
 									<input
 										type="text"
-										id="adoptionfee"
-										name="adoptionfee"
-										value={formData.adoptionFee}
-										onChange={handleInputChange}
+										id="adoptionFee"
+										name="adoptionFee"
+										placeholder={petDetails[0]?.adoptionFee}
+										onChange={(e) =>
+											setUpdates({
+												...updates,
+												adoptionFee: e.target.value,
+											})
+										}
 										className="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500"
 									/>
 								</div>
 								<button
+									onClick={() => saveUpdate()}
 									type="submit"
 									className="w-full bg-primary hover:bg-secondary text-white font-bold py-2 px-4 rounded"
 								>
@@ -149,17 +206,58 @@ const ManagePets = () => {
 						</div>
 					</div>
 				)}
+				{showDeleteModal && (
+					<div className="fixed top-0 left-0 w-full h-full flex items-center justify-center z-50">
+						<div className="absolute bg-white w-2/3 md:w-1/2 lg:w-1/3 p-6 rounded shadow-lg">
+							<span
+								onClick={toggleDeleteModal}
+								className="cursor-pointer text-2xl absolute top-0 right-0 p-4"
+							>
+								&times;
+							</span>
+							<h2 className="font-bold">Delete Pet</h2>
+							<div>
+								<div className="flex py-4">
+									<p className="">
+										{deleteSuccess
+											? 'Pet deleted successfully.'
+											: 'Are you sure you want to delete this pet?'}
+									</p>
+								</div>
+								<div className="flex justify-end">
+									{deleteSuccess ? (
+										<button
+											onClick={() => okay()}
+											type="submit"
+											className="mt-4 bg-primary hover:bg-secondary text-white font-bold py-2 px-4 rounded"
+										>
+											Okay
+										</button>
+									) : (
+										<button
+											onClick={() => deletePet()}
+											type="submit"
+											className="mt-4 bg-red-600 hover:bg-red-500 text-white font-bold py-2 px-4 rounded"
+										>
+											Delete
+										</button>
+									)}
+								</div>
+							</div>
+						</div>
+					</div>
+				)}
 			</div>
 			<div className="flex justify-between">
 				<h1 className="font-bold text-2xl text-slate-600">Manage Pets</h1>
 				<Link to={'/admin/manage-pets/add'}>
 					{' '}
-					<button className="bg-primary text-white px-4 py-1 rounded">
+					<button className="bg-green-600 hover:bg-green-700 text-sm text-white px-4 py-1 rounded">
 						Add New Pet
 					</button>
 				</Link>
 			</div>
-			<table className="w-full my-4 text-sm border  rounded-md text-left rtl:text-right text-gray-600">
+			<table className="w-full my-2 text-sm border  rounded-md text-left rtl:text-right text-gray-600">
 				<thead className="text-xs text-white uppercase bg-secondary">
 					<tr>
 						<th scope="col" className="px-6 py-3">
@@ -190,7 +288,7 @@ const ManagePets = () => {
 						<tr key={pet._id} className="bg-white border-b ">
 							<th
 								scope="row"
-								className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap"
+								className="px-6 py-4 text-md font-medium text-gray-900 whitespace-nowrap"
 							>
 								{pet.name}
 							</th>
@@ -199,7 +297,7 @@ const ManagePets = () => {
 							<td className="px-6 py-4">{pet.age}</td>
 							<td className="px-6 py-4">Php {pet.adoptionFee}.00</td>
 							<td className="px-6 py-4">
-								<div className="flex">
+								<div className="flex text-xs">
 									<p
 										className={`px-2 py-1 rounded text-white  ${
 											pet.availableForAdoption ? 'bg-green-600' : 'bg-red-600'
@@ -211,11 +309,21 @@ const ManagePets = () => {
 							</td>
 							<td className="px-6 py-4">
 								<div className="flex gap-x-2">
-									<button onClick={toggleModal}>
+									<button
+										onClick={() => {
+											setIdToEdit(pet._id);
+											toggleModal();
+										}}
+									>
 										<FaRegEdit color="#246b07" size={20} />
 										{/* Edit */}
 									</button>
-									<button>
+									<button
+										onClick={() => {
+											setIdToDelete(pet._id);
+											toggleDeleteModal();
+										}}
+									>
 										<MdDeleteOutline color="#b50e1a" size={20} />
 									</button>
 								</div>
